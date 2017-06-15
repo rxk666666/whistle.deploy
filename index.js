2e1b6f4cb7e749186afba5c9eb42925de39a4bc8
crash <posttobeget@gmail.com>
2017-06-15 11:49:40 +0800

    change git addr

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 module.exports = require('./lib/server');
 module.exports.uiServer = require('./lib/uiServer');
 module.exports.rulesServer = require('./lib/rulesServer');
-//module.exports.resRulesServer = require('./lib/resRulesServer');

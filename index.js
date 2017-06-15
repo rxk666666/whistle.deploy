556f7a517293d6a58c99158acb0f90ce52f20f7a
crash <posttobeget@gmail.com>
2017-06-15 11:46:43 +0800

    submit whistle plugin deploy

---
+++ index.js
@@ -0,0 +1,4 @@
+module.exports = require('./lib/server');
+module.exports.uiServer = require('./lib/uiServer');
+module.exports.rulesServer = require('./lib/rulesServer');
+//module.exports.resRulesServer = require('./lib/resRulesServer');
